381e0832f3f327b4d0b1715fe6f82fa609a5e5e9
Justin Oberdorf <joberdorf@acs-ilm.com>
2020-10-30 00:52:23 -0400

    Move configs Make configuration work with non standard locations. Should fix issue with failing to start on systemd

--- Graph2AutoTask/Program.cs
+++ Graph2AutoTask/Program.cs
@@ -1,23 +1,27 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Graph;
 using Serilog;
+using System.Diagnostics;
 
 namespace Graph2AutoTask
 {
     public class Program
     {
+        private static string _configuration_path = null;
         public static void Main(string[] args)
         {
+            _configuration_path = $"{System.IO.Path.GetDirectoryName(new System.Uri(System.Reflection.Assembly.GetExecutingAssembly().CodeBase).LocalPath)}/configs/";
             System.Console.WriteLine("Graph2AutoTask v2.0");
             System.Threading.Thread.CurrentThread.Name = "main";
             try
             {
                 System.Console.WriteLine("[LOGGING] Initializing");
                 IConfigurationRoot _logConfig = new ConfigurationBuilder()
-                    .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+                    .SetBasePath(_configuration_path)
                     .AddJsonFile("serilog.json")
                     .Build();
                 Log.Logger = new LoggerConfiguration()
@@ -28,6 +32,8 @@ namespace Graph2AutoTask
             catch(System.Exception _ex)
             {
                 System.Console.WriteLine("[LOGGING] Initialization Failed! Contact developer.");
+                System.Console.WriteLine($"Current Directory: {System.IO.Directory.GetCurrentDirectory()}");
+                System.Console.WriteLine($"Exception: {_ex.Message}");
                 return;
             }
             try { 
@@ -37,7 +43,8 @@ namespace Graph2AutoTask
             }
             catch(System.Exception _ex)
             {
-                Log.Error($"System exception during startup or shutdown. Provide details to developer.",_ex);
+                Log.Error(_ex, $"System exception during startup or shutdown. Provide details to developer.");
+                Log.Error($"Exception: {_ex.Message}");
             }
             finally
             {
@@ -50,6 +57,14 @@ namespace Graph2AutoTask
             AutomationConfig _configuration = new AutomationConfig();
 
             IHostBuilder _builder = Host.CreateDefaultBuilder(args)
+                 .ConfigureAppConfiguration(_configBuilder =>
+                    {
+                        PhysicalFileProvider _provider = new PhysicalFileProvider(_configuration_path);
+                        if (!System.Diagnostics.Debugger.IsAttached)
+                            _configBuilder.AddJsonFile(_provider, "appsettings.json", false, false);
+                        else
+                            _configBuilder.AddJsonFile(_provider, "appsettings.Development.json", false, false);
+                    })
                 .ConfigureServices((hostContext, services) =>
                 {
                     hostContext.Configuration.Bind(_configuration);
